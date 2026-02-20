58373103c99f103e6635e0566e280a08ac29af6a
roldanv79-crypto <roldanv79@gmail.com>
2026-02-20 12:14:34 -0400

    Add files via upload

---
+++ 1ro/js/main.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const navMobile = document.querySelector('.nav-mobile');
+
+    menu.classList.toggle('active');
+    navMobile.classList.toggle('active');
+} 
+
+// change background videos
+function changeVideo(name) {
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    const videoContainer = document.querySelectorAll('.video-container');
+
+    bgVideoList.forEach(video => {
+        video.classList.remove('active');
+        if (video.classList.contains(name)) {
+            video.classList.add('active');
+        }
+    });
+
+    videoContainer.forEach(video => {
+        video.classList.remove('active');
+        if (video.classList.contains(name)) {
+            video.classList.add('active');
+        }
+    });
+}
