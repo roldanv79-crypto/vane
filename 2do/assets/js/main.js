b94a94ba90714c7ae2caf945868d536fd279b53c
roldanv79-crypto <roldanv79@gmail.com>
2026-02-20 12:31:23 -0400

    Add files via upload

---
+++ 2do/assets/js/main.js
@@ -0,0 +1,12 @@
+
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+var swiper = new Swiper(".testimonial-wrapper", {
+    spaceBetween: 30,
+    loop: 'true',
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
