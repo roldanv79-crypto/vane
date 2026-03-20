50f8806b7e77ba5c3370679213593ae80fab3d4f
roldanv79-crypto <roldanv79@gmail.com>
2026-03-20 11:30:07 -0400

    Add files via upload

---
+++ tareas taller python.py
@@ -0,0 +1,52 @@
+# Escribe un programa que muestre tu nombre en la pantalla.
+nombre1="vanessa"
+print(nombre1)
+
+# Crea un programa que calcule y muestre el resultado de sumar 10 + 20.
+a = 10
+b = 20
+suma = a+b
+print(f"{suma}")
+
+# Escribe un programa que pregunte al usuario su nombre y luego lo salude con un "Hola, [nombre]".
+nombre= input ("ingresa nombre:")
+print(f"Hola, {nombre}")
+
+# Crea un programa que muestre tu comida favorita en la pantalla.
+comida="pizza"
+print(comida)
+
+# Escribe un programa que reste 5 a 15 y muestre el resultado.
+c = 15
+d = 5
+resta = c-d
+print(f"{resta}")
+
+# Crea un programa que pregunte al usuario cuántos años tiene y muestre el número en pantalla.
+
+edad= input ("cuantos años tienes:")
+print(f" {edad}")
+
+# Escribe un programa que muestre en pantalla el nombre de tu mascota o, si no tienes, el de una mascota que te gustaría tener.
+
+mascota= "pitufina"
+print(f" Mi mascota se llama {mascota}")
+
+# Crea un programa que multiplique 3 por 4 y muestre el resultado.
+e = 3
+f = 4
+multiplicacion = e*f
+print(f"{multiplicacion}")
+
+# Escribe un programa que pregunte al usuario cuál es su color favorito y responda "¡Qué buen gusto! El [color] es genial."
+
+color= input ("cual es su color favorito:")
+print(f" ¡Qué buen gusto! El {color} es genial")
+
+# Crea un programa que muestre la letra de tu canción favorita.
+
+print("--- Cielito Lindo ---")
+print("Ay, ay, ay, ay,")
+print("Canta y no llores,")
+print("Porque cantando se alegran,")
+print("Cielito lindo, los corazones.")
